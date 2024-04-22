9682a749d1c848ca032c5c7d89b32758e1298984
Nitin shukla <nitin.shukla12004@gmail.com>
2024-04-22 05:46:15 +0530

    Learned about using js with strict mode

--- OOPSinJs/strictmodeinjs.js
+++ OOPSinJs/strictmodeinjs.js
@@ -1,4 +1,15 @@
 'use strict'
 // After using above line, we are restrained to use let keyword with a. We can't write a=10 and console.log(a) to get the output.
-let a=10;
-console.log(a);
+
+// let a=10;
+// console.log(a);
+
+console.log(this);
+
+// "this" inside any method or function produces error.
+
+function displayname(){
+    console.log(this);
+}
+
+displayname();
