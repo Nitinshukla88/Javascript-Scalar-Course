1ed3082a0dda02ccb2a39cc39ee945bef06703d8
Nitin shukla <nitin.shukla12004@gmail.com>
2024-05-11 18:18:40 +0530

    Learned Destructuring in Js

--- Destructuring.js
+++ Destructuring.js
@@ -1,6 +1,7 @@
 let [a,b,c,d]=['My','name','is','Nitin'];
-// console.log(a,b,c,d);
-console.log(`${a}
-${b}
-${c}
-${d}`);
+console.log(a,b,c,d); // It works fine produce output on same line just same as python. To print on next line use "\n" or back-ticks(``) as below
+// console.log(`${a}
+// ${b}
+// ${c}
+// ${d}`);
+
