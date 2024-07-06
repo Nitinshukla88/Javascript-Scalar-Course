9fc85ffa136c8acb422b8fd92008794122ec010a
Nitin shukla <nitin.shukla12004@gmail.com>
2024-07-07 01:13:04 +0530

    Initialised the task given by Akshay Saini

---
+++ PromisesByAkshaySaini/Task1.js
@@ -0,0 +1,2 @@
+// Problem Statement: You are an E-commerce company. You have to make methods of adding things in cart after shopping, then proceed for payment, then you have to show the order summmary and also update the wallet of user.
+
