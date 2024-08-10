20cf5f298063889b2c7d0959ea44f2b449028d5c
Nitin shukla <nitin.shukla12004@gmail.com>
2024-08-11 02:35:00 +0530

    Wrote polyfills for call, apply

--- Polyfills.js
+++ Polyfills.js
@@ -7,12 +7,41 @@ let obj1 = {
     secondname: "Shukla",
 };
 
-function printname(){
-    console.log(this);
-    console.log(this.firstname + " " + this.secondname);
+function printname(gender, city){
+    console.log(this.firstname + " " + this.secondname + " is a "+ gender + " and live in "+ city);
 }
 
-printname.call(obj1);// standard call function........................
+// printname.call(obj1);// standard call function........................
 
 // Our polyfill- mycall()
+
+
+// Function.prototype.mycall = function(obj1, ...args){
+//     obj1.print = this;
+//     obj1.print(args[0], args[1]);
+// }
+
+// printname.mycall(obj1, "male", "kanpur"); 
+
+
+// printname.apply(obj1, ["male", "kanpur"]); // standard apply function......................
+
+// Our polyfill- myapply
+
+
+// Function.prototype.myapply = function(obj1, args){
+//     obj1.print = this;
+//     obj1.print(...args);
+// }
+
+// printname.myapply(obj1, ["male", "kanpur"]); 
+
+
+// Polyfill for bind- mybind
+
+// let myfunc = printname.bind(obj1, "male", "city"); // standard bind function....................
+
+// myfunc();
+
+// Our polyfill for bind- mybind
 
